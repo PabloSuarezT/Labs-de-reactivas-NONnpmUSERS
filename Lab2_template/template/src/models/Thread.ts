79c8f14bf24e08a5f2c10d6fbb3203042ba251af
PabloSuarezT <pablo.suarez@ug.uchile.cl>
2026-08-14 11:23:20 -0400

    Feat: Pequeño aporte para que reconozca la rama

--- Lab2_template/template/src/models/Thread.ts
+++ Lab2_template/template/src/models/Thread.ts
@@ -1,13 +1,21 @@
 import mongoose from "mongoose";
 
 export interface IThread {
-    /** defina aqui su interfaz */
+    autor: String;
+    contenido: String;
+    createAt?: Date;
+    updateAt?: Date;
+
 }
 
 // P1: Defina aquí los campos author y content como obligatorios, y configure
 // la creación automática de la fecha con timestamps o default: Date.now.
 const threadSchema = new mongoose.Schema<IThread>({
-    /* defina aqui sus campos */
+    autor: {
+        type: String,
+        required: [true, 'El campo autor es obligatorio'],
+        trim: true,
+    }
 });
 
 const Thread = mongoose.model<IThread>("Thread", threadSchema);
