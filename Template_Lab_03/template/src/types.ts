3e9ce3e5121d700b8293cee71ddc421db62df773
Bastian-AAI <bastian.alexis.arias.2005@gmail.com>
2026-08-21 18:03:47 -0400

    feat: lab 3 prerequisitos

---
+++ Template_Lab_03/template/src/types.ts
@@ -0,0 +1,46 @@
+export interface BakemonAbility {
+  name: string;
+  is_hidden: boolean;
+  slot: number;
+}
+
+export interface BakemonStat {
+  name: string;
+  base_value: number;
+  effort: number;
+}
+
+export interface BakemonType {
+  type: string;
+  slot: number;
+}
+
+export interface Bakemon {
+  id: number;
+  name: string;
+  abilities: BakemonAbility[];
+  height: number;
+  weight: number;
+  moves: number[];
+  stats: BakemonStat[];
+  types: BakemonType[];
+}
+
+export interface StatChange {
+  stat: string;
+  change: number;
+}
+
+export interface BakemonMove {
+  id: number;
+  name: string;
+  damage_class: string;
+  power: number | null;
+  pp: number;
+  priority: number;
+  stat_changes: StatChange[];
+  target: string;
+  type: string;
+  ailment: string;
+  effect_entry: string;
+}
