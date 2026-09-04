8592b015491a8e7ad6b8ea8c2b7935c7f172b692
Pozo6889 <secarvajal@ug.uchile.cl>
2026-09-04 13:33:46 -0400

    P1 final

--- Lab5_template/backend/src/models/post.ts
+++ Lab5_template/backend/src/models/post.ts
@@ -1,24 +1,25 @@
+///////////////////////    P1    ////////////////////////////////
+
 import mongoose from "mongoose";
 
-// Lista de autores no permitidos
 const FORBIDDEN_AUTHORS = ["Huevito rey", "Matías Toro", "Memes es mal ramo"];
 
 const postSchema = new mongoose.Schema({
   content: {
     type: String,
     required: true,
-    minLength: 1,   // Restricción: mínimo 1 caracter
-    maxLength: 300, // Restricción: máximo 300 caracteres
+    minLength: 1,   // Mínimo 1 caracter
+    maxLength: 300, // Máximo 300 caracteres
   },
   author: {
     type: String,
     default: "Anónimo",
     validate: {
-      // Validador personalizado: verifica que el nombre no esté en la lista negra
+      // Verifica que el autor no esté en la lista de prohibidos.
       validator: function (value: string) {
         return !FORBIDDEN_AUTHORS.includes(value);
       },
-      message: (props) => `${props.value} no es un nombre de autor permitido.`,
+      message: (props: { value: string }) => `${props.value} no es un nombre de autor permitido.`,
     },
   },
   thread: { type: Number, default: null },
